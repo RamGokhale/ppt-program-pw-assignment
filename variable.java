a06f6c7acb7603910556e5056fb4ffb9fc4328a1
RamGokhale <123485331+RamGokhale@users.noreply.github.com>
2023-07-02 20:52:17 +0530

    Add files via upload

---
+++ variable.java
@@ -0,0 +1,18 @@
+public class Variables {
+
+    public static void main(String[] args) {
+        // Declare and initialize an int variable
+        int myAge = 30;
+
+        // Declare and initialize a double variable
+        double myGPA = 3.5;
+
+        // Declare and initialize a String variable
+        String myName = "Bard";
+
+        // Print the values of the variables
+        System.out.println("My age is " + myAge);
+        System.out.println("My GPA is " + myGPA);
+        System.out.println("My name is " + myName);
+    }
+}
